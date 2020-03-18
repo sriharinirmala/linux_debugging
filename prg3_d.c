f8312565a90696d379581f642a966be486d59117
sriharinirmala <49486723+sriharinirmala@users.noreply.github.com>
2020-03-18 14:00:10 +0530

    Add files via upload

---
+++ prg3_d.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(void)
+{
+    char *p;
+    char c[50]="sri";
+     p = &c; 
+
+    printf("%s",c); 
+
+    return 0;
+}
---
+++ prg4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(void)
+{
+    char *p = malloc(1);
+    *p = 'a'; 
+
+    char c = *p; 
+
+    printf("\n [%c]\n",c); 
+
+    free(p);
+    c = *p;
+    return 0;
+}
+
